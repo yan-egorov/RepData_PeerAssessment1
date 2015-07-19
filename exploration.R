e926ea4ecf0b898c49315c1b1cb77efe5d2b727b
yan-egorov <egorov.yan@gmail.com>
2015-07-19 21:10:54 +0300

    Data exploration comleted and Rmd file created

--- exploration.R
+++ exploration.R
@@ -1,48 +1,22 @@
----
-title: "PA1_template.Rmd"
-author: "Yan Egorov"
-date: "19 июля 2015 г."
-output: html_document
----
-
-Before start exploring our data we have to include all required packages with the following command:
-```{r}
 library(lattice)
-```
 
-Then we read and take a very first look at the file structure:
-```{r}
 data <- read.csv(file = "activity.csv")
 head(data)
 str(data)
-```
 
-Let's calculate and examine total number of steps taken per each day:
-```{r}
 steps_by_day <- aggregate(steps ~ date, data = data, FUN = sum)
+str(steps_by_day)
 hist(steps_by_day$steps,breaks = 20)
-```
-
-The mean and median values of the total number of steps taken per day:
-```{r}
 mean(steps_by_day$steps)
 median(steps_by_day$steps)
-```
 
-Let's now examine mean number of steps taken by each 5-minute interval:
-```{r}
 steps_by_time <- aggregate(steps ~ interval, data = data, FUN = mean)
 plot(x = steps_by_time$interval, 
      y = steps_by_time$steps, 
      type ='l')
 max_steps_arg = which.max(steps_by_time$steps)
 max_steps_time <- steps_by_time$interval[max_steps_arg]
-max_steps_time
-```
-We can see the highest activity at 8:35 AM
 
-In the data we've explored we have a lot of missing values. Let's count them and replace it with the mean values within each time interval (you can uncomment the string to controll replacing process).
-```{r}
 summary(data$steps)["NA's"]
 data_tidy <- data
 for (iter in 1:nrow(data_tidy))
@@ -52,32 +26,22 @@ for (iter in 1:nrow(data_tidy))
         time_interval <- data_tidy$interval[iter]
         mean_for_time <- steps_by_time[steps_by_time$interval==time_interval,"steps"]
         data_tidy$steps[iter] <- mean_for_time
-        #cat(iter,time_interval,mean_for_time,'\n', sep=' ### ')
+        cat(iter,time_interval,mean_for_time,'\n', sep=' ### ')
         } 
     }
 summary(data_tidy$steps)
-```
 
-As we can see there is no NA's in the new dataset called **data_tidy**.
-Let's check how our replacement affects data statistics:
-```{r}
 steps_by_day_filled <- aggregate(steps ~ date, data = data_tidy, FUN = sum)
-hist(steps_by_day_filled$steps,breaks = 20,main = 'Histogram after NAs replacement')
-mean(steps_by_day_filled$steps)
-median(steps_by_day_filled$steps)
-```
+hist(steps_by_day_filled$steps,breaks = 20)
+mean(steps_by_day$steps)
+median(steps_by_day$steps)
 
-To explore how weekday impact on human activity we want to mark weekends in our dataset:
-```{r}
 data_tidy$weekday <- weekdays(as.Date(data_tidy$date))
 data_tidy$weekends <- ifelse(data_tidy$weekday == "Суббота" | 
                              data_tidy$weekday == "Воскресенье", "Weekend", "Weekday")
-```
 
-Finally we calculate mean number of steps for each time interval separately for weekends and workdays:
-```{r}
 steps_by_time_weekend_flag <- aggregate(data_tidy$steps, 
-                                        by = list(data_tidy$interval, data_tidy$weekends),
+                                        by = list(data_tidy$interval, data_tidy$weekends), 
                                         FUN=mean)
 colnames(steps_by_time_weekend_flag) <- c("interval", "weekend_flag", "steps")
 xyplot(steps ~ interval | weekend_flag, 
@@ -88,4 +52,3 @@ xyplot(steps ~ interval | weekend_flag,
        ylab = "Number of steps", 
        main = "Activity Patterns on Weekends and Weekdays", 
        col = "steelblue")
-```
